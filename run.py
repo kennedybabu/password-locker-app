70484db251f312a2e0b5f050c0a517caaecd160b
kennedybabu <kennedynjogu8@gmail.com>
2021-10-24 19:37:15 +0300

    save user

---
+++ run.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3.8
+from unittest import result
+from user import User
+from credentials import Credentials
+import random
+import string
+import sys
+
+
+def create_user(fname, lname, password):
+    """
+    Function that will create a new user
+    """
+    new_user = User(fname, lname,password)
+    return new_user
+
+def save_user(user):
+    """
+    Function to save user
+    """
+    user.save_user()
+
+
+def create_user_credentials(platform_name, platform_username, platform_password):
+    """
+    Function that will create a new credential
+    """
+    new_user_credentials = Credentials(platform_name, platform_username, platform_password)
+    return new_user_credentials
